edf048f71cf0a7fd78c56a822b8678bd08848a3f
Goumies <contact@romyalula.com>
2019-02-21 15:00:03 +0100

    Extract updateDefaultItem method condition about aged brie to 1st if

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -10,31 +10,30 @@ class GildedRose {
     void updateQuality() {
         for (Item item : items) {
             if ("Aged Brie".equals(item.name)) {
-
+                if (item.quality < 50) {
+                    item.quality = item.quality + 1;
+                }
             } else {
-                if (!"Aged Brie".equals(item.name)
-                        && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
-                    }
-                } else {
+                if ("Backstage passes to a TAFKAL80ETC concert".equals(item.name)) {
                     if (item.quality < 50) {
                         item.quality = item.quality + 1;
 
-                        if ("Backstage passes to a TAFKAL80ETC concert".equals(item.name)) {
-                            if (item.sellIn < 11) {
-                                if (item.quality < 50) {
-                                    item.quality = item.quality + 1;
-                                }
+                        if (item.sellIn < 11) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
+                        }
 
-                            if (item.sellIn < 6) {
-                                if (item.quality < 50) {
-                                    item.quality = item.quality + 1;
-                                }
+                        if (item.sellIn < 6) {
+                            if (item.quality < 50) {
+                                item.quality = item.quality + 1;
                             }
+                        }
+                    }
+                } else {
+                    if (item.quality > 0) {
+                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                            item.quality = item.quality - 1;
                         }
                     }
                 }
@@ -44,7 +43,11 @@ class GildedRose {
                 }
 
                 if (item.sellIn < 0) {
-                    if (!"Aged Brie".equals(item.name)) {
+                    if ("Aged Brie".equals(item.name)) {
+                        if (item.quality < 50) {
+                            item.quality = item.quality + 1;
+                        }
+                    } else {
                         if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                             if (item.quality > 0) {
                                 if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
@@ -53,10 +56,6 @@ class GildedRose {
                             }
                         } else {
                             item.quality = item.quality - item.quality;
-                        }
-                    } else {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
                         }
                     }
                 }
